22956dcaa46e06e5481c99f7386058f431260659
Nayan Khemka <nayankhemka9@gmail.com>
2020-10-11 07:10:24 +0530

    update6

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Wait till the DOM is ready.
 $(function () {
-    var roles = ["Student", "VITian", "Artist"];
+    var roles = ["Student", "VITian", "Learner","Activist"];
     var count = 0;
     setInterval(() => {
       $("#intro .content h2 span").fadeOut("fast", function () {
